1997f9c12349019194b8255c77faf3930570c6a3
David Alonso <david.alonsobadia@gmail.com>
2023-06-29 16:54:45 +0200

    Update terraform.tf
    Co-authored-by: Sergi Alonso <45431066+sergialonsaco@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,2 +1,16 @@
 terraform {
+  backend "s3" {}
+
+  required_version = ">= 1.3.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
 }
